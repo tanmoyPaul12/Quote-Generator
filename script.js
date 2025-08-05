4f0b45d41c0b1c1469181853f2f694b335a0a7e1
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-08-06 01:36:09 +0530

    done

--- script.js
+++
@@ -1,8 +0,0 @@
-const api_url ="https://api.quotable.io/random";
-async function getquote(url) {
-    const response = await fetch(url);
-    var data = response.json();
-    console.log(data);
-}
-
-getquote(api_url);
